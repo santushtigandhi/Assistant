04f9fe1d49570e931ea5068a6f34f78a6a98df6d
unknown <santushti.gandhi@gmail.com>
2022-06-04 22:16:03 +0530

    Initial Push

---
+++ assistant.py
@@ -0,0 +1,98 @@
+import pyttsx3
+import speech_recognition as sr 
+import datetime
+import wikipedia
+import webbrowser 
+import os
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def greetMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Google Assistant. Please tell me how may I help you")       
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+if __name__ == "__main__":
+    greetMe()
+    while True:
+        query = takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            speak("Opening Youtibe")
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            speak("Opening google")
+            webbrowser.open("google.com")
+
+        elif 'instagram' in query:
+            speak("Opening Instagram")
+            webbrowser.open("instagram.com")   
+
+        elif 'facebook' in query:
+            speak("Opening Facebook")
+            webbrowser.open("facebook.com")
+        elif 'whatsapp' in query:
+            speak("Opening Whatsapp")
+            webbrowser.open("web.whatsapp.com")
+            
+        elif 'play music' in query:
+            music_dir = "C:\\Users\\ACER\\Music"
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[1]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Ma'am, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\ACER\\AppData\\Local\\Programs\\Microsoft VS Code\Code.exe"
+            os.startfile(codePath)
+
+        elif "exit" in query or "close" in query or "stop" in query:
+            speak("Thank you. Have a nice day! Bye")
+            break
+        else:
+            speak("Sorry! I didn't recognise that! Try again.")
+        
